63dd9ab6350b3c589120a677a6c651758c2311f7
Agustina Quinteros <142256460+Agustinaqui@users.noreply.github.com>
2023-11-11 11:51:42 -0300

    agrego nuevos endpoints

--- router.php
+++ router.php
@@ -7,24 +7,28 @@ require_once 'app/controllers/estilos.api.controller.php';
 
 $router = new Router();
 
-                //endpoint  //verbo  //controller        //metodo
-$router->addRoute('cervezas', 'GET', 'CervezasApiController', 'get');
-$router->addRoute('cervezas', 'POST', 'CervezasApiController', 'create');
-$router->addRoute('cervezas/:ID', 'GET', 'CervezasApiController', 'get');
-$router->addRoute('cervezas/:ID', 'PUT', 'CervezasApiController', 'update');
-$router->addRoute('cervezas/:ID', 'DELETE', 'CervezasApiController', 'delete');
-$router->addRoute('cervezas/:ID/:subrecurso', 'GET', 'CervezasApiController', 'get');
 
-$router->addRoute('estilos', 'GET', 'EstilosApiController', 'get');
-$router->addRoute('estilos', 'POST', 'EstilosApiController', 'create');
-$router->addRoute('estilos/:ID', 'GET', 'EstilosApiController', 'get');
-$router->addRoute('estilos/:ID', 'PUT', 'EstilosApiController', 'update');
+                //endpoint                    //verbo   //controller             //metodo
+$router->addRoute('cervezas',                 'GET',    'CervezasApiController', 'get');
+$router->addRoute('cervezas',                 'POST',   'CervezasApiController', 'create');
+$router->addRoute('cervezas/:ID',             'GET',    'CervezasApiController', 'get');
+$router->addRoute('cervezas/:ID',             'PUT',    'CervezasApiController', 'update');
+$router->addRoute('cervezas/:ID',             'DELETE', 'CervezasApiController', 'delete');
+$router->addRoute('cervezas/:ID/:subrecurso', 'GET',    'CervezasApiController', 'get');
+
+                  //endpoint     //verbo   //controller            //metodo
+$router->addRoute('estilos',     'GET',    'EstilosApiController', 'get');
+$router->addRoute('estilos',     'POST',   'EstilosApiController', 'create');
+$router->addRoute('estilos/:ID', 'GET',    'EstilosApiController', 'get');
+$router->addRoute('estilos/:ID', 'PUT',    'EstilosApiController', 'update');
 $router->addRoute('estilos/:ID', 'DELETE', 'EstilosApiController', 'delete');
 
-
+                  //endpoint         //verbo   //controller                //metodo
+$router->addRoute('comentarios',     'GET',    'ComentariosApiController', 'get');
+$router->addRoute('comentarios/:ID', 'GET',    'ComentariosApiController', 'get');
+$router->addRoute('comentarios/:ID', 'DELETE', 'ComentariosApiController', 'delete');
+$router->addRoute('comentarios',     'POST',   'ComentariosApiController', 'create');
 
 $router->addRoute('user/token', 'GET', 'UserApiController', 'getToken');
-
-
 
 $router->route($_GET['resource'], $_SERVER['REQUEST_METHOD']);
